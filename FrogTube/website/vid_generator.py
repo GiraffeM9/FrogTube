2f339a84dfbfe225e304eb7ba272ef48234fa1fe
GiraffeM9 <76228570+GiraffeM9@users.noreply.github.com>
2021-11-15 22:13:37 +0000

    Add files via upload

--- FrogTube/website/vid_generator.py
+++ FrogTube/website/vid_generator.py
@@ -1,26 +1,47 @@
 import googleapiclient.discovery
 import random
+from os import path
+import json
 
-api_service_name, api_version, DEVELOPER_KEY = "youtube", "v3", "insert developer key"
+api_service_name, api_version, DEVELOPER_KEY = "youtube", "v3", "AIzaSyD-698vmi9ufJjfOS0xp3Ew-Vme7sDTn_s"
+file_path = "website/templates/static/videos.json"
 
-def generate_video():
-    videos = []
-
+def api_response():
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
-    request = youtube.search().list(
-        part="snippet",
-        maxResults=300,
-        q="frog",
-        safeSearch="strict",
-        type="video",
-        videoEmbeddable="true"
-    )
-    response = request.execute()
+    tags = ["frog", "frogs", "cute frog", "funny frog"]
+    r = {}
+    for tag in tags:
+        request = youtube.search().list(
+            part="snippet",
+            maxResults=1000,
+            q=tag,
+            safeSearch="strict",
+            type="video",
+            videoEmbeddable="true"
+        )
+        response = request.execute()
+        r.update(response)
+    with open(file_path, "w") as file:
+                json.dump(r, file)
+    return response
+    
 
-    for result in response.get('items', []):
-        if result['id']['kind'] == 'youtube#video':
-            videos.append('%s' % (result['id']['videoId']))
+def generate_video():
+    if not path.exists(file_path):
+        response = api_response()
+    else:
+        with open(file_path, "r") as file:
+            response = json.load(file)
+    videos = []
+    for result in response.get("items", []):
+        if result["id"]["kind"] == "youtube#video":
+            videos.append('%s' % (result["id"]["videoId"]))
+    random.shuffle(videos)
     # print(generate_video())
     return (random.choice(videos))
+
+
+def get_videos(num):
+    pass
